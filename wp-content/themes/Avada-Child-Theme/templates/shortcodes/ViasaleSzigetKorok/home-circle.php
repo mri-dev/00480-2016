2689e459959e29bf429e5e39b0a18b49ddffaeda
mistvan22 <pocokxtrame@gmail.com>
2016-09-14 13:12:14 +0200

    Sziget körök v1 - FINAL

--- wp-content/themes/Avada-Child-Theme/templates/shortcodes/ViasaleSzigetKorok/home-circle.php
+++ wp-content/themes/Avada-Child-Theme/templates/shortcodes/ViasaleSzigetKorok/home-circle.php
@@ -1,6 +1,12 @@
 
 <div class="circle circle-<?php echo $post_name; ?>">
   <div class="circle-inside">
-    <div class="title"><?php echo $post_title; ?></div>
+    <? $url   = get_permalink($ID); ?>
+    <? $image = get_the_post_thumbnail_url($ID, 'full'); if($image): ?>
+    <div class="image">
+      <a href="<?php echo $url;?>"><img src="<?php echo $image; ?> " alt="<?php echo $post_title; ?>" class="trans-on" /></a>
+    </div>
+    <? endif; ?>
+    <div class="title trans-on"><?php echo $post_title; ?></div>
   </div>
 </div>
